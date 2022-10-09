54e48c123fdf40e3ae15a287b9ac2158d25c5a63
Hiro0608 <h.satou1991@gmail.com>
2022-10-09 19:18:49 +0900

    tweetsコントローラー、モデル作成

--- db/migrate/20221009041948_devise_create_users.rb
+++ db/migrate/20221009041948_devise_create_users.rb
@@ -4,8 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :nickname, null: false
-      t.string :firstname, null: false
-      t.string :lastname, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
       t.date   :birthday, null: false
       ## Database authenticatable
       t.string :email,              null: false, default: ""
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_09_042906) do
+ActiveRecord::Schema.define(version: 2022_10_09_093831) do
 
   create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "provider"
@@ -21,10 +21,20 @@ ActiveRecord::Schema.define(version: 2022_10_09_042906) do
     t.index ["user_id"], name: "index_sns_credentials_on_user_id"
   end
 
+  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "place", null: false
+    t.text "introduction", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
-    t.string "firstname", null: false
-    t.string "lastname", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.date "birthday", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -38,4 +48,5 @@ ActiveRecord::Schema.define(version: 2022_10_09_042906) do
   end
 
   add_foreign_key "sns_credentials", "users"
+  add_foreign_key "tweets", "users"
 end
