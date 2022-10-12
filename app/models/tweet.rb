24fadeb268beab859ea09a85c8c4a99dc0dc7c14
Hiro0608 <h.satou1991@gmail.com>
2022-10-12 17:14:14 +0900

    Active Storageの導入

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,10 @@
 class Tweet < ApplicationRecord
   belongs_to :user, optional: true
+  has_one_attached :image
+  #has_many :comments, dependent: :destroy
+
+  validates :title, presence: true
+  validates :place, presence: true
+  validates :introduction, presence: true  
+  validates :image, presence: true
 end
