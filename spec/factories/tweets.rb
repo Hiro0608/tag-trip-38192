671baa040b06e30153a3acfb6aef2f0e4d00dc81
Hiro0608 <h.satou1991@gmail.com>
2022-10-15 23:22:03 +0900

    投稿機能テストコード

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :tweet do
+    title          { Faker::Lorem.word }
+    place          { Faker::Lorem.word }
+    introduction   { Faker::Lorem.sentence }
     
+    association :user
+    after(:build) do |tweet|
+      tweet.image.attach(io: File.open('public/images/test_image.jpeg'), filename: "test_image.jpeg")
+    end
   end
 end
